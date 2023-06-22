7676b2873bc1d51ab95cc394fa28e42926cb90a8
alefra88 <efra.rlz@gmail.com>
2023-06-22 16:22:17 -0600

    adding features to ./controllers/products.js

--- controllers/products.js
+++ controllers/products.js
@@ -3,13 +3,26 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({
-    name: "vase table",
+    page: "2",
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  } else {
+  }
+  if (company) {
+    queryObject.company = company;
+  } else {
+  }
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 
