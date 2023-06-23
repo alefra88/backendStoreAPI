0a62441906011e14a5af13f185765f3e19ed60ce
alefra88 <efra.rlz@gmail.com>
2023-06-23 12:46:58 -0600

    aaa

--- controllers/products.js
+++ controllers/products.js
@@ -2,9 +2,7 @@ const product = require("../models/product");
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    /*name: { $regex: search, $options: "i"*/
-  }).select("name price");
+  const products = await Product.find({}).sort("name").select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -41,6 +39,13 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   } else {
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  //23
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
