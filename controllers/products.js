f978fbeb965a1ef8983f899e90cbd1f01f4aaf16
alefra88 <efra.rlz@gmail.com>
2023-06-23 11:21:59 -0600

    fields

--- controllers/products.js
+++ controllers/products.js
@@ -4,12 +4,12 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({
     /*name: { $regex: search, $options: "i"*/
-  }).sort("-name price");
+  }).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -27,11 +27,19 @@ const getAllProducts = async (req, res) => {
   console.log(queryObject);
 
   let result = Product.find(queryObject);
+
+  //sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("CreateAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  } else {
   }
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
